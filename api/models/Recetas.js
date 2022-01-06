dea9159ef2057bcede2e7b84877ce9e69f950bd2
Pablo Fontecilla <pafonbu@gmail.com>
2022-01-06 16:12:25 -0300

    CHANGE: separa variavles entorno github segun ambiente. ADD: en modelo de mongo indicar los campos requeridos.

--- api/models/Recetas.js
+++ api/models/Recetas.js
@@ -5,30 +5,30 @@ const Receta = mongoose.model(
   "receta",
   new Schema(
     {
-      correlativo: Number,
-      numeroReceta: Number,
-      tipoReceta: Number,
-      rutPaciente: String,
-      medicoPrescriptor: String,
+      correlativo: { type: Number, require: true },
+      numeroReceta: { type: Number, require: true },
+      tipoReceta: { type: Number, require: true },
+      rutPaciente: { type: String, require: true },
+      medicoPrescriptor: { type: String, require: true },
       patologia: String,
-      recetaRetenida: Boolean,
-      fechaEmision: String,
-      codigoEstablecimiento: String,
-      nombreEstablecimiento: String,
+      recetaRetenida: { type: Boolean, default: false },
+      fechaEmision: { type: String, require: true },
+      codigoEstablecimiento: { type: String, require: true },
+      nombreEstablecimiento: { type: String, require: true },
       pases: [
         {
-          numeroReceta: Number,
-          fechaEmisionFutura: String,
-          numeroPase: Number,
+          numeroReceta: { type: Number, require: true },
+          fechaEmisionFutura: { type: String, require: true },
+          numeroPase: { type: Number, require: true },
         },
       ],
-      medicamentos: [
+      medicamentos: { type: [
         {
-          nombreMedicamento: String,
-          medicamentoControlado: Boolean,
-          mensaje: String,
+          nombreMedicamento: { type: String, require: true },
+          medicamentoControlado: { type: Boolean, default: false },
+          mensaje: { type: String, require: true },
         },
-      ],
+      ], require: true }
     },
     { timestamps: true },
   )
